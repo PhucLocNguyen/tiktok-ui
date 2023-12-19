ab245b53b269aea1b61edfb7b6049df6db510932
PhucLocNguyen <loc19992003bca@gmail.com>
2023-12-19 14:00:39 +0700

    Bai 1 : cai Babel chinh sua Config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
+import Button from '~/components/Button/button';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button/>
       </header>
     </div>
   );
---
+++ src/components/Button/button.js
@@ -0,0 +1,7 @@
+import React from "react";
+import myAlert from "~/alert";
+function Button(){
+return  <button onClick={()=>myAlert()}>Click Now</button>
+
+}
+export default Button;
